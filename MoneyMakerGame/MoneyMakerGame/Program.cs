faeffa24ed92d7ecbcfd9b14bdaacb306b433699
joanarfc <joanarfcarv@gmail.com>
2023-06-19 19:24:27 -0300

    Code refactoration

--- MoneyMakerGame/MoneyMakerGame/Program.cs
+++ MoneyMakerGame/MoneyMakerGame/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MoneyMakerGame
 {
@@ -12,22 +8,44 @@ namespace MoneyMakerGame
         {
             Console.WriteLine("Welcome to Money Maker!");
 
+            double amount = ReadValidAmount();
+
+            if (amount != -1)
+            {
+                Console.WriteLine($"{amount} cents is equal to...");
+
+                int goldCoin = 10;
+                int silverCoin = 5;
+
+                double goldCoins = Math.Floor(amount / goldCoin);
+                double remainder = amount % goldCoin;
+
+                double silverCoins = Math.Floor(remainder / silverCoin);
+                remainder = amount % silverCoin;
+
+                Console.WriteLine($"Gold coins: {goldCoins}");
+                Console.WriteLine($"Silver coins: {silverCoins}");
+                Console.WriteLine($"Bronze coins: {remainder}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid amount.");
+            }
+
+            Console.ReadLine();
+        }
+
+        static double ReadValidAmount()
+        {
             Console.WriteLine("Enter the amount of money you wish to convert:");
-            double amount = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"{amount} cents is equal to...");
-
-            int goldCoin = 10;
-            int silverCoin = 5;
-
-            double goldCoins = Math.Floor(amount / goldCoin);
-            double remainder = amount % goldCoin;
-
-            double silverCoins = Math.Floor(remainder / silverCoin);
-            remainder = amount % silverCoin;
-
-            Console.WriteLine($"Gold coins: {goldCoins}");
-            Console.WriteLine($"Silver coins: {silverCoins}");
-            Console.WriteLine($"Bronze coins: {remainder}");
+            if (double.TryParse(Console.ReadLine(), out double amount))
+            {
+                return amount;
+            }
+            else
+            {
+                return -1;
+            }
         }
     }
 }
